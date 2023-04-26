73bd75dfca69a7c0fdc62628794ad91a73e65ab1
Matthias Brucklacher <matthias.brucklacher@gmail.com>
2023-04-26 11:30:05 +0200

    Enlarge axis label

--- scripts/post_run_analysis/plot_fig5cd.py
+++ scripts/post_run_analysis/plot_fig5cd.py
@@ -30,10 +30,10 @@ def load_network_accuracy(resultfolder, architectures, n_ipc_list, decode_mode):
         acc_means.update({networkIt: []}) 
         acc_std.update({networkIt: []}) 
         for n_ipcIt in n_ipc_list:
-            with open(resultfolder+f'/arch-{architectures[networkIt]}_nipc-{n_ipcIt}/metrics/linear_decoding_accuracy_{decode_mode}_mnist_extended_fast_noise-0_static-0.txt', "rb") as fp:
+            with open(resultfolder+f'fig5cd_arch-{architectures[networkIt]}_nipc-{n_ipcIt}/metrics/linear_decoding_accuracy_{decode_mode}_mnist_extended_fast_noise-0_static-None.txt', "rb") as fp:
                 list = pickle.load(fp)
                 acc_means[networkIt].append(list[-1]) # take value at last training epoch
-            with open(resultfolder+f'/arch-{architectures[networkIt]}_nipc-{n_ipcIt}/metrics/linear_decoding_accuracy_{decode_mode}_std_mnist_extended_fast_noise-0_static-0.txt', "rb") as fp:
+            with open(resultfolder+f'fig5cd_arch-{architectures[networkIt]}_nipc-{n_ipcIt}/metrics/linear_decoding_accuracy_{decode_mode}_std_mnist_extended_fast_noise-0_static-None.txt', "rb") as fp:
                 list = pickle.load(fp)
                 acc_std[networkIt].append(list[-1]) # take value at last training epoch
     return acc_means, acc_std
@@ -49,8 +49,6 @@ def make_input_data_3d(data):
     return data.reshape(shape[0] * shape[1] * shape[2], shape[3], shape[4] * shape[5])
 
 if __name__ == '__main__':
-
-
     # Load data for baseline models
     trafos = [0] # Used transformations
     data = torch.tensor(np.load('../data/data_preprocessed/mnist_extended_fast.npy'))[:, :, trafos] # Shape is (10, 50, 1, 6, 34, 34)
@@ -79,15 +77,15 @@ if __name__ == '__main__':
         sfa_decoding_accuracy_validate.append(sfa_validate)
 
         # Baseline 2: K-means
-        kmeans_train, kmeans_validate = kmeans_decoding(
-            train_array=data[:, [n_ipc]],
-            train_labels=data_labels[:, [n_ipc]],
-            validate_array=test_data,
-            validate_labels=test_labels,
-            n_clusters=10
-        )
-        kmeans_decoding_accuracy_train.append(kmeans_train)
-        kmeans_decoding_accuracy_validate.append(kmeans_validate)
+        # kmeans_train, kmeans_validate = kmeans_decoding(
+        #     train_array=data[:, [n_ipc]],
+        #     train_labels=data_labels[:, [n_ipc]],
+        #     validate_array=test_data,
+        #     validate_labels=test_labels,
+        #     n_clusters=10
+        # )
+        # kmeans_decoding_accuracy_train.append(kmeans_train)
+        # kmeans_decoding_accuracy_validate.append(kmeans_validate)
 
         # Baseline 3: Linear readout
         linear_decoding_accuracy_train.append( 
@@ -112,19 +110,19 @@ if __name__ == '__main__':
     # Prepare plotting
     fig1, ax1 = plt.subplots()
     fig2, ax2 = plt.subplots()
-    ax1.set_ylabel ('Decoding accuracy')
-    ax2.set_ylabel('Validation decoding accuracy')
+
     label_fontsize = 16
     ticklabel_fontsize = label_fontsize
     title_fontsize = label_fontsize + 3 
     legend_fontsize = label_fontsize - 2
-    legend_frameon = False
+    ax1.set_ylabel ('Decoding accuracy', fontsize=label_fontsize)
+    ax2.set_ylabel('Validation decoding accuracy', fontsize=label_fontsize)
 
     # Plot network performance
     architectures = {'net_1': '[2000-500-30]', 'net_2' : '[4000-2000-90]'}
     colors = {'net_1': 'tab:green', 'net_2': 'tab:cyan'}
     plot_labels = {'net_1': 'Original net size', 'net_2': 'Increased net size'}
-    resultfolder = '../results/intermediate/fig5cd'
+    resultfolder = '../results/intermediate/'
     network_accuracy_validate_mean, network_accuracy_validate_std = load_network_accuracy(resultfolder, architectures, n_ipc_list, decode_mode='validate')
     network_accuracy_train_mean, network_accuracy_train_std = load_network_accuracy(resultfolder, architectures, n_ipc_list, decode_mode='train_classes')
     for networkIt in architectures:
@@ -135,8 +133,8 @@ if __name__ == '__main__':
     ax1.plot(n_samples_list, sfa_decoding_accuracy_train, label='SFA inputs', linewidth=2, linestyle='-.', color='k')
     ax2.plot(n_samples_list, sfa_decoding_accuracy_validate, label='SFA inputs', linewidth=2, linestyle='-.', color='k')
 
-    ax1.plot(n_samples_list, kmeans_decoding_accuracy_train, label='k-means inputs', linewidth=2, linestyle ='--', color ='k')
-    ax2.plot(n_samples_list, kmeans_decoding_accuracy_validate, label='k-means inputs', linewidth=2, linestyle ='--', color ='k')
+    # ax1.plot(n_samples_list, kmeans_decoding_accuracy_train, label='k-means inputs', linewidth=2, linestyle ='--', color ='k')
+    # ax2.plot(n_samples_list, kmeans_decoding_accuracy_validate, label='k-means inputs', linewidth=2, linestyle ='--', color ='k')
     
     ax1.plot(n_samples_list, linear_decoding_accuracy_train, label='LD inputs', linewidth=2, color='k', linestyle=':')
     ax2.plot(n_samples_list, linear_decoding_accuracy_validate, label='LD inputs', linewidth=2, color='k', linestyle=':')
