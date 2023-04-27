d19a744c02c20002da43eb5fc65fbfefbbcc50a7
Matthias Brucklacher <matthias.brucklacher@gmail.com>
2023-04-27 12:44:55 +0200

    Cosmetic fix

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,51 +7,38 @@ Functions to load data and labels as well as to add noise to them.
 
 def load_data(dataset, n_instances_per_class_train, trafos, labels_filename, device):
 
-    data_path = f'../data/data_preprocessed/{dataset}'
-    data_original_format = torch.tensor(np.load(data_path), device = device) # (samples, sequencelength, dim_x, dim_y)
+    PATH_TO_DATA = f'../data/data_preprocessed/{dataset}'
+    data = torch.tensor(np.load(PATH_TO_DATA), device=device)
     print("Data " + dataset + " loaded successfully")
-    data_width = data_original_format.shape[-1]
 
-    selected_trafos = list(set(trafos)) # Keep only unique values #[0,1,2] # List to specify which transformations are included: 0 - translation, 1 - rotation, 2 - scaling
-    if labels_filename != 'smallnorb_labels.npy': # Smallnorb dataset as used here does not contain instances per class to create a validation dataset. 
+    selected_trafos = list(set(trafos)) # Keep only unique values #[0, 1, 2] # List to specify which transformations are included: 0 - translation, 1 - rotation, 2 - scaling
+    if labels_filename in ['smallnorb_labels.npy', 'labels_autocorrelation_mnist.npy']: # These datasets do not contain instances per class to create a validation dataset. 
+        n_instances_per_class_validate = 0 
+        print('X')
+    else: 
         n_instances_per_class_validate = 20 # Don't change (standardized validation set)
-    else: n_instances_per_class_validate = 0 
     print(str(n_instances_per_class_train) + ' instances per class in training data')
-    print(str(n_instances_per_class_validate)+ ' instances per class in validation data')
+    print(str(n_instances_per_class_validate) + ' instances per class in validation data')
     n_instances_selected = n_instances_per_class_train + n_instances_per_class_validate # Training + validation
 
-    assert all(trafo in list(range(data_original_format.shape[2])) for trafo in selected_trafos), 'Selected transformations must be present in dataset' 
-    assert n_instances_selected <= data_original_format.shape[1], "Number of instances selected ({}) must be smaller than total number of instances in dataset ({})".format(n_instances_selected, data_original_format.shape[1])
-    data_original_format = data_original_format[:,:,selected_trafos]
-    data_original_format = torch.flatten(data_original_format, start_dim = -2)  # Flatten last two dimensions (image width and height). Shape is now [n_classes, n_trafos, n_instances, height*width]
-    data = torch.flatten(data_original_format, end_dim = -3)
+    assert all(trafo in list(range(data.shape[2])) for trafo in selected_trafos), 'Selected transformations must be present in dataset' 
+    assert n_instances_selected <= data.shape[1], "Number of instances selected ({}) must be smaller than total number of instances in dataset ({})".format(n_instances_selected, data.shape[1])
+    data = data[:, :, selected_trafos]
+    data = torch.flatten(data, start_dim=-2)  # Flatten last two dimensions (image width and height). Shape is now [n_classes, n_trafos, n_instances, height*width]
 
-    labels_path = f'../data/data_preprocessed/{labels_filename}'
-    data_labels_original_format = np.load(labels_path)
-    data_labels_original_format = data_labels_original_format[:,:,selected_trafos]
+    PATH_TO_LABELS = f'../data/data_preprocessed/{labels_filename}'
+    labels = np.load(PATH_TO_LABELS)
+    labels = labels[:, :, selected_trafos]
 
-    assert data_original_format.shape[0:3] == data_labels_original_format.shape[0:3], "Dimension of labels must match dimension of data"
+    assert data.shape[0:3] == labels.shape[0:3], "Dimension of labels must match dimension of data"
 
-    """
-    Split into training and validation subsets
-    """
+    # Split into training and validation subsets.
+    validate_labels = labels[:, -n_instances_per_class_validate:, :] # take last 20 samples
+    validate_data = data[:, -n_instances_per_class_validate:, :]
+    train_labels = labels[:, :n_instances_per_class_train, :]
+    train_data = data[:, :n_instances_per_class_train, :]
 
-    # Two views of each dataset (useful later). In the "_original_format", the dimensions of n_classes, n_trafos, n_instances are maintained
-    validate_labels_original_format = data_labels_original_format[:,-n_instances_per_class_validate:,:] # take last 20 samples
-    validate_data_original_format = data_original_format[:,-n_instances_per_class_validate:, :]
-    train_labels_original_format = data_labels_original_format[:, :n_instances_per_class_train, :]
-    train_data_original_format = data_original_format[:, :n_instances_per_class_train, :]
-
-    # Historic dataset of rotating digits. In the current manuscript version, this is only used for the autocorrelation results, and requires pretrained weights.
-    load_old_data = False
-    if load_old_data:
-        train_data_original_format = torch.tensor(np.load('rotData.npy'), device = device)
-        data_width = train_data_original_format.shape[-1]
-        train_data_original_format = torch.flatten(train_data_original_format, start_dim = -2)
-        train_data_original_format = train_data_original_format[:, None, None, :, :]
-        validate_data_original_format = train_data_original_format
-        validate_labels_original_format = train_labels_original_format
-    return train_data_original_format, validate_data_original_format, train_labels_original_format, validate_labels_original_format
+    return train_data, validate_data, train_labels, validate_labels
 
 def add_noise(frame, device):
     """Takes input image and fills empty parts with random noise.
